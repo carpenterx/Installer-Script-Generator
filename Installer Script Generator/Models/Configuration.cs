4c2a3990d05e024cc611fc1a229c7f00b821c588
Sorin <jordache_sor@yahoo.com>
2021-09-05 21:53:10 +0300

    added folder name converter

--- Installer Script Generator/Models/Configuration.cs
+++ Installer Script Generator/Models/Configuration.cs
@@ -1,4 +1,9 @@
-﻿namespace Installer_Script_Generator.Models
+﻿using System.Globalization;
+using System.Windows.Data;
+using System;
+using System.IO;
+
+namespace Installer_Script_Generator.Models
 {
     public class Configuration
     {
@@ -7,12 +12,26 @@
         public string FileExtension { get; set; }
         public string FileType { get; set; }
 
-        public Configuration(string releasePath, string version, string fileExtension, string fileType)
+        public Configuration(string releasePath, string version, string fileExtension = "", string fileType = "")
         {
             ReleasePath = releasePath;
             Version = version;
             FileExtension = fileExtension;
             FileType = fileType;
+        }
+    }
+
+    public class PathToFolderNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string folderPath = (string)value;
+            return Path.GetFileName(folderPath);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
